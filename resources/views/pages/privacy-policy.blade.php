53526e078b63fe39f8a762e4ce8716d5da1c77b1
buihongquan <buihongquan@vnpt.vn>
2023-10-11 00:01:07 +0700

    fix bug detail post

--- resources/views/pages/privacy-policy.blade.php
+++ resources/views/pages/privacy-policy.blade.php
@@ -4,14 +4,14 @@ Chính sách bảo mật
 @endsection
 @section('content')
 <main class="privacy-policy text-center">
-  <section class="mx-auto privacy-title">
-    <p class="m-0 text-font-size-36">Chính Sách Bảo Mật Thông Tin Cá Nhân Khách Hàng</p>
+  <section class="container edupia-mw-1230 mx-auto privacy-title">
+    <p class="m-0 text-font-size-40">Chính Sách Bảo Mật Thông Tin Cá Nhân Khách Hàng</p>
   </section>
   <section class="container edupia-mw-1230 text-left">
     <div class="row">
       <div class="col">
-        <div>
-          <p class="text-font-size-24 text-blue-300 text-font-weight-600 pl-1">
+        <div class="mb-4">
+          <p class="text-font-size-24 text-blue-300 text-font-weight-600 pl-1 mb-3">
             1. Mục đích và phạm vi thu nhập thông tin
           </p>
           <div class="text-font-size-18 text-font-weight-400">
@@ -21,63 +21,63 @@ Chính sách bảo mật
             <p>
               Các khách hàng sẽ tự chịu trách nhiệm về tính chính xác và trung thực của các thông tin khi cung cấp cho Edupia.vn (họ và tên, email, số điện thoại và địa chỉ của mình). Ngoài ra, khách hàng có trách nhiệm thông báo kịp thời cho Edupia.vn về những hành vi sử dụng trái phép, lạm dụng, vi phạm bảo mật để có biện pháp giải quyết phù hợp.
             </p>
-            <p>
+            <p class="mb-0">
               Khi khách hàng đăng ký khóa học, khách hàng sẽ được thêm vào danh sách nhận tin tức, đề xuất đặc biệt hoặc ưu đãi không thường xuyên từ Edupia.vn.
             </p>
           </div>
         </div>
 
         <div>
-          <p class="text-font-size-24 text-blue-300 text-font-weight-600 pl-1">
+          <p class="text-font-size-24 text-blue-300 text-font-weight-600 pl-1 mb-3">
             2. Phạm vi sử dụng thông tin
           </p>
-          <div class="text-font-size-18 text-font-weight-400">
+          <div class="text-font-size-18 text-font-weight-400 mb-6">
             <p>
               Website Edupia.vn sử dụng thông tin khách hàng cung cấp để:
             </p>
             <ul class="text-font-size-18 text-font-weight-400 pl-4">
               <li>
-                <p>
+                <p class="mb-2">
                   Tư vấn cho khách hàng qua điện thoại miễn phí về dịch vụ liên quan tới chương trình học ngôn ngữ áp dụng phương pháp giáo dục sớm cho trẻ em.
                 </p>
               </li>
               <li>
-                <p>
+                <p class="mb-2">
                   Giải đáp thắc mắc khách hàng.
                 </p>
               </li>
               <li>
-                <p>
+                <p class="mb-2">
                   Gửi các thông báo về các hoạt động trao đổi thông tin giữa khách hàng và Edupia.vn.
                 </p>
               </li>
               <li>
-                <p>
+                <p class="mb-2">
                   Thực hiện các bản khảo sát khách hàng.
                 </p>
               </li>
               <li>
-                <p>
+                <p class="mb-2">
                   Thực hiện các hoạt động quảng bá liên quan đến các khóa học.
                 </p>
               </li>
               <li>
-                <p>
+                <p class="mb-2">
                   Ngăn ngừa các hoạt động phá hủy thông tin của khách hàng hoặc các hoạt động giả mạo khách hàng.
                 </p>
               </li>
               <li>
-                <p>
+                <p class="mb-2">
                   Liên lạc và giải quyết với khách hàng trong những trường hợp đặc biệt.
                 </p>
               </li>
               <li>
-                <p>
+                <p class="mb-2">
                   Không sử dụng thông tin cá nhân của khách hàng ngoài mục đích xác nhận và liên hệ có liên quan đến giao dịch tại Edupia.vn.
                 </p>
               </li>
               <li>
-                <p>
+                <p class="mb-0">
                   Trong trường hợp có yêu cầu của pháp luật: Edupia.vn có trách nhiệm hợp tác cung cấp thông tin cá nhân khách hàng khi có yêu cầu từ cơ quan tư pháp bao gồm: Viện kiểm sát, tòa án, cơ quan công an điều tra liên quan đến hành vi vi phạm pháp luật nào đó của khách hàng. Ngoài ra, không ai có quyền xâm phạm vào thông tin cá nhân của khách hàng.
                 </p>
               </li>
@@ -85,50 +85,50 @@ Chính sách bảo mật
           </div>
         </div>
 
-        <div>
-          <p class="text-font-size-24 text-blue-300 text-font-weight-600 pl-1">
+        <div class="mb-4">
+          <p class="text-font-size-24 text-blue-300 text-font-weight-600 pl-1 mb-3">
             3. Thời gian lưu trữ thông tin
           </p>
           <div class="text-font-size-18 text-font-weight-400">
             <p>
               Dữ liệu cá nhân của khách hàng sẽ được lưu trữ cho đến khi Edupia.vn nhận được yêu cầu hủy bỏ, xóa. Còn lại trong mọi trường hợp thông tin cá nhân của khách hàng sẽ được bảo mật trên máy chủ của Edupia.vn.
             </p>
-            <p>
+            <p class="mb-0">
               Edupia.vn sẽ lưu giữ và sử dụng thông tin của khách hàng khi cần tuân theo nghĩa vụ pháp lý, giải quyết tranh chấp và thực thi các thoả thuận của Edupia.vn.
             </p>
           </div>
         </div>
 
-        <div>
-          <p class="text-font-size-24 text-blue-300 text-font-weight-600 pl-1">
+        <div class="mb-3">
+          <p class="text-font-size-24 text-blue-300 text-font-weight-600 pl-1 mb-3">
             4. Địa chỉ của đơn vị thu thập và quản lý thông tin cá nhân
           </p>
           <div class="text-font-size-18 text-font-weight-400">
             <p>
               Khách hàng có quyền yêu cầu hủy bỏ, xóa thông tin cá nhân của mình bằng cách yêu cầu ban quản trị website Edupia.vn thực hiện việc này.
             </p>
-            <p>
+            <p class="mb-0">
               Khách hàng có quyền gửi khiếu nại về việc lộ thông tin cá nhân cho bên thứ 3 đến Ban quản trị của website Edupia.vn. Khi tiếp nhận những phản hồi này, Edupia.vn sẽ xác nhận lại thông tin, phải có trách nhiệm trả lời lý do và thông báo cho khách hàng cách bảo mật lại thông tin.
             </p>
           </div>
         </div>
 
-        <div>
-          <p class="text-font-size-24 text-blue-300 text-font-weight-600 pl-1">
+        <div class="mb-4">
+          <p class="text-font-size-24 text-blue-300 text-font-weight-600 pl-1 mb-3">
             5. Phương tiện và công cụ để người dùng tiếp cận và chỉnh sửa dữ liệu cá nhân của mình
           </p>
           <div class="text-font-size-18 text-font-weight-400">
             <p>
               Khách hàng có quyền yêu cầu hủy bỏ, xóa thông tin cá nhân của mình bằng cách yêu cầu ban quản trị website Edupia.vn thực hiện việc này.
             </p>
-            <p>
+            <p class="mb-0">
               Khách hàng có quyền gửi khiếu nại về việc lộ thông tin cá nhân cho bên thứ 3 đến Ban quản trị của website Edupia.vn. Khi tiếp nhận những phản hồi này, Edupia.vn sẽ xác nhận lại thông tin, phải có trách nhiệm trả lời lý do và thông báo cho khách hàng cách bảo mật lại thông tin.
             </p>
           </div>
         </div>
        
         <div>
-          <p class="text-font-size-24 text-blue-300 text-font-weight-600 pl-1">
+          <p class="text-font-size-24 text-blue-300 text-font-weight-600 pl-1 mb-3">
             6. Cam kết bảo mật thông tin cá nhân khách hàng
           </p>
           <div class="text-font-size-18 text-font-weight-400">
@@ -147,7 +147,7 @@ Chính sách bảo mật
             <p>
               Bảo mật tuyệt đối mọi thông tin giao dịch trực tuyến của khách hàng bao gồm thông tin hóa đơn kế toán chứng từ số hóa tại khu vực dữ liệu trung tâm an toàn cấp 1 của Edupia.vn.
             </p>
-            <p>
+            <p class="mb-0">
               Ban quản lý website Edupia.vn yêu cầu các cá nhân khi đăng ký/mua hàng là khách hàng, phải cung cấp đầy đủ thông tin cá nhân có liên quan gồm: Họ và tên, địa chỉ, email, điện thoại và chịu trách nhiệm về tính pháp lý của những thông tin trên. Ban quản lý website Edupia.vn không chịu trách nhiệm cũng như không giải quyết mọi khiếu nại có liên quan đến quyền lợi của Khách hàng đó nếu xét thấy tất cả thông tin cá nhân của khách hàng đó cung cấp khi đăng ký ban đầu là không chính xác.
             </p>
           </div>
